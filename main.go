48c0a1779e8aae9f9bae24178c3e30496e850cba
pigdin <38335749+pigdin@users.noreply.github.com>
2026-04-14 18:23:02 -0700

    oops a token

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Token:", token)
+}
